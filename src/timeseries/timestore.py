b0dc4f78727e542e004d174d69b57247a48f24ed
Niall McCarroll <niall.mccarroll@gmail.com>
2023-07-27 17:42:28 +0100

    updates

--- src/timeseries/timestore.py
+++ src/timeseries/timestore.py
@@ -9,11 +9,16 @@ class TimeStore:
 
     FILLVALUE = -32768
 
-    def __init__(self, filepath, year, nj, ni, dtype=np.int16, scale=1.0, offset=0.0):
+    def __init__(self, filepath, year, nj, ni, dtype=np.int16, scale=1.0, offset=0.0, period="daily"):
         self.year = year
         self.filepath = filepath
         self.dtype = dtype
         self.array = None
+        self.period = period
+        if self.period == "daily":
+            self.shape = (12,nj,ni,31)
+        else:
+            self.shape = (nj,ni,12)
         self.nj = nj
         self.ni = ni
         self.scale = scale
@@ -28,32 +33,38 @@ class TimeStore:
                 self.valid_indexes.append(idx)
         print(len(self.valid_indexes))
 
+    def get_period(self):
+        return self.period
+
     def open(self):
         if not os.path.exists(self.filepath):
             self.array = np.memmap(filename=self.filepath, dtype=self.dtype, mode="w+", offset=0,
-                                   shape=(12, self.nj, self.ni, 31), order="F")
+                                   shape=self.shape, order="C")
             self.array[:, :, :, :] = -32768
         else:
-            self.array = np.memmap(filename=self.filepath, dtype=self.dtype, mode="r+", shape=(12, self.nj, self.ni, 31),
-                                   offset=0, order="F")
+            self.array = np.memmap(filename=self.filepath, dtype=self.dtype, mode="r+", shape=self.shape,
+                                   offset=0, order="C")
 
-    def add(self, month, day, data):
-        self.array[month,:,:,day] = data
+    def add_day(self, month, day, data):
+        self.array[month,:,:,day] = np.where(np.isnan(data), TimeStore.FILLVALUE, np.int16((data-self.offset)/self.scale))
+
+    def add_month(self, month, data):
+        self.array[:,:,month] = data
 
     def get(self, j, i):
-        values = self.array[:,j,i,:].flatten().tolist()
-        decoded = []
-        for idx in self.valid_indexes:
-            if values[idx] == TimeStore.FILLVALUE:
-                decoded.append(None)
-            else:
-                decoded.append(values[idx]*self.scale + self.offset)
-        return decoded
+        if self.period == "daily":
+            values = self.array[:,j,i,:]
+        else:
+            values = self.array[j,i,:]
+        values = np.where(values == -32768, np.nan, values*self.scale + self.offset)
+        values = values.flatten()[self.valid_indexes]
+        return values.tolist()
+
 
     def save(self):
         self.array.flush()
 
-def test_load(ts,frompath):
+def tsload(ts,frompath):
     for month in sorted(os.listdir(frompath)):
         monthpath = os.path.join(frompath, month)
         for day in sorted(os.listdir(monthpath)):
@@ -65,14 +76,14 @@ def test_load(ts,frompath):
             print("adding:" + filepath)
             monthidx = int(month) - 1
             dayidx = int(day) - 1
-            ds = xr.open_dataset(filepath, mask_and_scale=False)
+            ds = xr.open_dataset(filepath)
             sst = ds["analysed_sst"].data
-            ts.add(monthidx, dayidx, sst)
+            ts.add_day(monthidx, dayidx, sst)
 
 
 if __name__ == '__main__':
-    ts = TimeStore("/data/esacci_sst/ts.npy",2021,3600,7200,scale=0.01,offset=273.15)
+    ts = TimeStore("/data/esacci_sst/ts2.npy",2021,3600,7200,scale=0.01,offset=273.15)
     ts.open()
-    # test_load(ts,"/data/esacci_sst/public/CDR3.0_release/Analysis/L4/v3.0.1/2021")
-    # ts.save()
+    tsload(ts,"/data/esacci_sst/public/CDR3.0_release/Analysis/L4/v3.0.1/2021")
+    ts.save()
     print(ts.get(600,50))
